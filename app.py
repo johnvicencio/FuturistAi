3c35a137f93625e95d441e81c1b74cf1c3ad7312
John Vicencio <jvicencio@johnvicencio.com>
2025-06-02 22:25:24 -0700

    Changed static permission

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ from datetime import datetime
 from utils.astro_helpers import calculate_life_path_number, get_chinese_zodiac, fetch_astrological_signs
 from utils.prompt_helpers import native_astro_prediction
 
-app = Flask(__name__)
+from flask import Flask, url_for
+
+app = Flask(__name__, static_folder='static', static_url_path='/static')
 
 @app.route("/", methods=["GET", "POST"])
 def index():
