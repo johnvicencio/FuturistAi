1b1c4129d6170c0c302220b582674cbe3f4c74fc
John Vicencio <jvicencio@johnvicencio.com>
2025-06-02 17:23:07 -0700

    Updated to fix js error regarding modal

--- static/js/app.js
+++ static/js/app.js
@@ -1,11 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
-  var predictionModal = new mdb.Modal(document.getElementById('predictionModal'));
-  predictionModal.show();
+  var modalEl = document.getElementById('predictionModal');
+  if (modalEl) {
+    var predictionModal = new mdb.Modal(modalEl);
+    predictionModal.show();
 
-  var doneButtons = document.querySelectorAll('.done-button');
-  doneButtons.forEach(function (button) {
-    button.addEventListener('click', function () {
-      window.location.href = '/';
+    var doneButtons = document.querySelectorAll('.done-button');
+    doneButtons.forEach(function (button) {
+      button.addEventListener('click', function () {
+        window.location.href = '/';
+      });
     });
-  });
+  }
 });
